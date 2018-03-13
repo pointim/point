0b4b9ffb8733b7e7cc6b8ca6de32178111ad6e2c
Arts <arts@red.psto.net>
2018-03-13 13:19:09 +0000

    Database

--- init.sh
+++ init.sh
@@ -6,4 +6,7 @@ done
 
 mkdir -p img
 mkdir -p postgresql
-mkdir -p redis/{storage,cache,queue,pubsub}
+mkdir -p redis/{storage,cache,sessions,queue,pubsub}
+
+docker build -t point-os -f ./Dockerfile.os .
+docker build -t point-db -f ./Dockerfile.db .

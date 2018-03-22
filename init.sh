c4d3bd9a1a4543e8e5e48f7ab7bc622d52cb99b6
Arts <arts@red.psto.net>
2018-03-22 19:04:31 +0000

    run prosody in docker

--- init.sh
+++ init.sh
@@ -5,8 +5,8 @@ for i in core doc feed imgproc stat www xmpp; do
 done
 
 mkdir -p img
-mkdir -p postgresql
-mkdir -p redis/{storage,cache,sessions,queue,pubsub,imgproc}
+mkdir -p data/postgresql
+mkdir -p data/redis/{storage,cache,sessions,queue,pubsub,imgproc}
 mkdir -p ssl
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./ssl/server.key -out ./ssl/server.crt

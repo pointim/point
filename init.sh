87303cb6414e78bf3e140dc5706c29e183952008
Arts <arts@red.psto.net>
2018-03-25 19:07:49 +0000

    move dev dump into repo

--- init.sh
+++ init.sh
@@ -7,7 +7,5 @@ mkdir -p ssl
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./ssl/server.key -out ./ssl/server.crt
 
-wget -c https://point.im/files/dump.sql.gz -O ./docker-entrypoint-initdb.d/dump.sql.gz
-
 docker build --rm -t point-os -f ./Dockerfile.os .
 docker build --rm -t point-db -f ./Dockerfile.db .

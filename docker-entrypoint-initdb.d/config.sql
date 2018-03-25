47231ace82cb5be53d8541185a353b2c956c2a22
Arts <arts@red.psto.net>
2018-03-25 20:32:43 +0000

    tune postgres

---
+++ docker-entrypoint-initdb.d/config.sql
@@ -0,0 +1,30 @@
+ALTER SYSTEM SET
+ max_connections = '200';
+ALTER SYSTEM SET
+ shared_buffers = '4GB';
+ALTER SYSTEM SET
+ effective_cache_size = '12GB';
+ALTER SYSTEM SET
+ maintenance_work_mem = '1GB';
+ALTER SYSTEM SET
+ min_wal_size = '1GB';
+ALTER SYSTEM SET
+ max_wal_size = '2GB';
+ALTER SYSTEM SET
+ checkpoint_completion_target = '0.7';
+ALTER SYSTEM SET
+ wal_buffers = '16MB';
+ALTER SYSTEM SET
+ default_statistics_target = '100';
+ALTER SYSTEM SET
+ random_page_cost = '1.1';
+ALTER SYSTEM SET
+ effective_io_concurrency = '200';
+ALTER SYSTEM SET
+ max_worker_processes = '4';
+ALTER SYSTEM SET
+ max_parallel_workers_per_gather = '2';
+ALTER SYSTEM SET
+ max_parallel_workers = '4';
+ALTER SYSTEM SET
+ work_mem = '5242kB';

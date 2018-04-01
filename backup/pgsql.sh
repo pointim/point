c58f2f4c66b47c0835e348dce0db1386ff336fcb
Arts <arts@point.im>
2018-04-01 15:25:34 +0000

    postgres backup on host

--- backup/pgsql.sh
+++ backup/pgsql.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker exec it point_db_1 pg_dump -h localhost -U point point | gzip - > "$2/pgsql-$1.sql.gz"
+#docker exec -it point_db_1 
+pg_dump -h localhost -U point point | gzip - > "$2/pgsql-$1.sql.gz"

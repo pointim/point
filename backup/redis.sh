6d882d4585951e781ba113ec601cf8851f6811f7
Arts <arts@point.im>
2018-04-01 15:26:20 +0000

    redis backup

--- backup/redis.sh
+++ backup/redis.sh
@@ -3,8 +3,8 @@
 redis_dir="../data/redis"
 
 for s in $(ls -1 $redis_dir); do
-echo == $s
-  echo docker exec -it "point_redis-${s}_1" redis-cli 'SAVE'
+  echo $s SAVE
+  docker exec -it "point_redis-${s}_1" redis-cli 'SAVE'
 done
 
 tar cjf "$2/redis-$1.tar.bz2" $redis_dir
